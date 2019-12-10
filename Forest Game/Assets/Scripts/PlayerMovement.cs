d537f9b5cbeaffaba21278c47aa83ba8f17f5416
AsgerHeinricy <asger.heinricy@gmail.com>
2019-12-10 15:49:20 +0100

    Small Updates

--- Forest Game/Assets/Scripts/PlayerMovement.cs
+++ Forest Game/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,16 @@ public class PlayerMovement : MonoBehaviour
     {
         Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
         Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.Space))
             Rigid.AddForce(transform.up * JumpForce);
+
+        if (Input.GetKey(KeyCode.W) && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            MoveSpeed = 0.4f;
+        }
+        else
+        {
+            MoveSpeed = 0.2f;
+        }
     }
 }
